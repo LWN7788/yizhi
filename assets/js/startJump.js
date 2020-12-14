c85c9e9fc1af58ac9394b70ac3ca360e83194795
liujinyuan <liujinyuan@sunvua.com>
2020-12-14 17:28:08 +0800

    jump

--- assets/js/startJump.js
+++ assets/js/startJump.js
@@ -9,13 +9,13 @@ cc.Class({
    
     onLoad () {
         var jumpAction = this.runJumpAction();
-        cc.tween(this.node).then(jumpAction).repeat(2).start()
+        cc.tween(this.node).then(jumpAction).repeat(3).start()
     },
     runJumpAction () {
         // 跳跃上升
-        var jumpUp = cc.tween().to(1, {y: 20}, {easing: 'sineOut'});
+        var jumpUp = cc.tween().by(0.2, {y: 20}, {easing: 'sineIn'});
         // 下落
-        var jumpDown = cc.tween().to(1, {y: 0}, {easing: 'sineIn'});
+        var jumpDown = cc.tween().by(1, {y: -20}, {easing: 'sineOut'});
 
         // 创建一个缓动，按 jumpUp、jumpDown 的顺序执行动作
         var tween = cc.tween().sequence(jumpUp, jumpDown)
